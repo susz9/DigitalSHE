b32e348663f3f56675e911687d149982c77fa9e5
sumit <sumitghr40@gmail.com>
2022-06-19 18:15:55 +0545

    logo added

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { google, slack, atlassian, dropbox, shopify } from './imports';
+import logo1 from '../../images/iHub.jpg'
+import logo2 from '../../images/Khwopa TU.png'
+import logo3 from '../../images/Khwopa.png'
+import logo4 from '../../images/premier-logo.png'
 import './brand.css';
 
 const Brand = () => (
@@ -9,20 +13,20 @@ const Brand = () => (
     </div> */}
     <div className="gpt3__brand section__padding">
       <div>
-        <img src={google} />
+        <img src={logo1} />
+      </div>
+      <div style={{
+        background: 'grey'
+      }}>
+        <img src={logo2} />
+      </div>
+      <div >
+        <img src={logo3} className="round-logo"/>
       </div>
       <div>
-        <img src={slack} />
+        <img src={logo4} />
       </div>
-      <div>
-        <img src={atlassian} />
-      </div>
-      <div>
-        <img src={dropbox} />
-      </div>
-      <div>
-        <img src={shopify} />
-      </div>
+      
     </div>
   </div>
 );
